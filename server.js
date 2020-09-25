f64fb34c7f67d7e2854963eb7dfb2115e15e911e
alexkhazzam <alex722khazzam@gmail.com>
2020-09-25 09:08:45 -0400

    adding stuff

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
-const express = require("express");
-const app = express();
-const http = 5500;
+const Express = require("express");
+const app = Express();
+const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const path = require("path");
 
 const users = {};
 
-app.use(express.static(`${__dirname}/public`));
+app.use(Express.static(`${__dirname}/public`));
 
-app.use("/", (req, res, next) => {
-  res.sendFile(path.join(`index.html`));
-  io.on("connection", (socket) => {
-    socket.on("new-user", (name) => {
-      users[socket.id] = name;
-      socket.broadcast.emit("user-connected", name);
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
     });
-    socket.on("send-chat-message", (message) => {
-      socket.broadcast.emit("chat-message", {
-        message: message,
-        name: users[socket.id],
-      });
-    });
-    socket.on("user-disconnected", () => {
-      socket.broadcast.emit("user-disconnected", users[socket.id]);
-      delete users[socket.id];
-    });
+  });
+  socket.on("user-disconnected", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
   });
 });
 
-io.listen(5000);
+http.listen(5000);
